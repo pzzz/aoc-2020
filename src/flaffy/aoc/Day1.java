f05915bdf8f6d5a3380761bf9c9b4ae254ae5d7a
Peter Schuller <peter.schuller@cas.de>
2020-12-01 09:34:27 +0100

    [wip] pair programmed partial solution for day 1 task 1

--- src/flaffy/aoc/Day1.java
+++ src/flaffy/aoc/Day1.java
@@ -1,5 +1,33 @@
 package flaffy.aoc;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Day1 {
+    public static void main(String[] args) throws Exception {
+        Day1 d1 = new Day1();
+        d1.task1();
+    }
+
+    public void task1() throws Exception {
+        List<String> allLines = Files.readAllLines(Paths.get("data/peter/day1input"));
+        int[] allExpenses = new int[allLines.size()];
+        for (int i = 0; i < allExpenses.length; i++) {
+            allExpenses[i] = Integer.parseInt(allLines.get(i));
+        }
+        System.out.println(calc(allExpenses));
+    }
+
+    public int calc(int[] numbers) throws Exception {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == 2020) {
+                    return numbers[i] * numbers[j];
+                }
+            }
+        }
+        throw new Exception("No matching expenses found!");
+    }
 
 }
