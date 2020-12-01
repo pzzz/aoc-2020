9956fd02d5c2552810acdf2aa5cdf3a79b75fdce
Peter Schuller <peter.schuller@cas.de>
2020-12-01 10:08:46 +0100

    [wip] working solution for day 1 task 2

--- src/flaffy/aoc/Day1.java
+++ src/flaffy/aoc/Day1.java
@@ -7,23 +7,44 @@ import java.util.List;
 public class Day1 {
     public static void main(String[] args) throws Exception {
         Day1 d1 = new Day1();
-        d1.task1();
+        d1.doTasks();
     }
 
-    public void task1() throws Exception {
-        List<String> allLines = Files.readAllLines(Paths.get("data/peter/day1input"));
+    public void doTasks() throws Exception {
+        List<String> allLines = Files.readAllLines(Paths.get("data/ulrike/day1input"));
         int[] allExpenses = new int[allLines.size()];
         for (int i = 0; i < allExpenses.length; i++) {
             allExpenses[i] = Integer.parseInt(allLines.get(i));
         }
-        System.out.println(calc(allExpenses));
+        System.out.println("Ulrike: " + calc1(allExpenses));
+        System.out.println("Ulrike: " + calc2(allExpenses));
+        allLines = Files.readAllLines(Paths.get("data/peter/day1input"));
+        allExpenses = new int[allLines.size()];
+        for (int i = 0; i < allExpenses.length; i++) {
+            allExpenses[i] = Integer.parseInt(allLines.get(i));
+        }
+        System.out.println("Peter: " + calc1(allExpenses));
+        System.out.println("Peter: " + calc2(allExpenses));
     }
 
-    public int calc(int[] numbers) throws Exception {
+    public int calc1(int[] numbers) throws Exception {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 1; j < numbers.length; j++) {
                 if (numbers[i] + numbers[j] == 2020) {
                     return numbers[i] * numbers[j];
+                }
+            }
+        }
+        throw new Exception("No matching expenses found!");
+    }
+
+    public int calc2(int[] numbers) throws Exception {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 1; j < numbers.length; j++) {
+                for (int k = 2; k < numbers.length; k++) {
+                    if (numbers[i] + numbers[j] + numbers[k] == 2020) {
+                        return numbers[i] * numbers[j] * numbers[k];
+                    }
                 }
             }
         }
