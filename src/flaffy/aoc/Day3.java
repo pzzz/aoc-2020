6ad35bb2ee3ac3822b0de4f1bdf99cdf29a3ad7a
Peter Schuller <peter.schuller@cas.de>
2020-12-07 16:29:50 +0100

    [wip] day 3 task 2

--- src/flaffy/aoc/Day3.java
+++ src/flaffy/aoc/Day3.java
@@ -13,26 +13,40 @@ public class Day3 {
     public void doTasks() throws Exception {
         List<String> allLines = Files.readAllLines(Paths.get("data/ulrike/day3input"));
         System.out.println("Ulrike: " + calc1(allLines));
-        // System.out.println("Ulrike: " + calc2(allLines));
+        System.out.println("Ulrike: " + calc2(allLines));
         allLines = Files.readAllLines(Paths.get("data/peter/day3input"));
         System.out.println("Peter: " + calc1(allLines));
-        // System.out.println("Peter: " + calc2(allLines));
+        System.out.println("Peter: " + calc2(allLines));
     }
 
     public int calc1(List<String> allLines) {
+        return check(allLines, 1, 3);
+    }
+
+    public int check(List<String> allLines, int down, int right) {
         int count = 0;
         int pos = 0;
-        for (String line: allLines) {
-            if (line.charAt(line % pos) == '#') {
+        for (int i = 0; i < allLines.size(); i+=down) {
+            String line = allLines.get(i);
+            int realPos = 0;
+            if (pos != 0) {
+                realPos = pos % line.length();
+            }
+            if (line.charAt(realPos) == '#') {
                 count++;
             }
-            pos += 3;
+            pos += right;
         }
         return count;
     }
 
     public int calc2(List<String> allLines) {
-        int count = 0;
-        return count;
+        int oneOne = check(allLines, 1, 1);
+        int threeOne = check(allLines, 1, 3);
+        int fiveOne = check(allLines, 1, 5);
+        int sevenOne = check(allLines, 1, 7);
+        int oneTwo = check(allLines, 2, 1);
+        int result = oneOne*threeOne*fiveOne*sevenOne*oneTwo;
+        return result;
     }
 }
