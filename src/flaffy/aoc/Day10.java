8ad47afdd709dfb6f6098b5c9e697a7b8379c215
Peter Schuller <peter.schuller@cas.de>
2020-12-10 14:38:05 +0100

    [wip] cleans up day 10 code

--- src/flaffy/aoc/Day10.java
+++ src/flaffy/aoc/Day10.java
@@ -16,34 +16,29 @@ public class Day10 {
 
     public void doTasks() throws Exception {
         List<String> allLines = Files.readAllLines(Paths.get("data/ulrike/day10input"));
+        List<Integer> allNumbers = getAllNumbers(allLines);
+        System.out.println("Ulrike: " + calc1(allNumbers));
+        System.out.println("Ulrike: " + calc2(allNumbers));
+        allLines = Files.readAllLines(Paths.get("data/peter/day10input"));
+        allNumbers = getAllNumbers(allLines);
+        System.out.println("Peter: " + calc1(allNumbers));
+        System.out.println("Peter: " + calc2(allNumbers));
+    }
+
+    public List<Integer> getAllNumbers(List<String> allLines) {
         List<Integer> allNumbers = new ArrayList<>();
         for (String line : allLines) {
             allNumbers.add(Integer.parseInt(line));
         }
-        System.out.println("Ulrike: " + calc1(allNumbers));
-        allNumbers = new ArrayList<>();
-        for (String line : allLines) {
-            allNumbers.add(Integer.parseInt(line));
-        }
-        System.out.println("Ulrike: " + calc2(allNumbers));
-        allLines = Files.readAllLines(Paths.get("data/peter/day10input"));
-        allNumbers = new ArrayList<>();
-        for (String line : allLines) {
-            allNumbers.add(Integer.parseInt(line));
-        }
-        System.out.println("Peter: " + calc1(allNumbers));
-        allNumbers = new ArrayList<>();
-        for (String line : allLines) {
-            allNumbers.add(Integer.parseInt(line));
-        }
-        System.out.println("Peter: " + calc2(allNumbers));
+        allNumbers.add(0);
+        Collections.sort(allNumbers);
+        allNumbers.add(allNumbers.get(allNumbers.size() - 1) + 3);
+        return allNumbers;
     }
 
     public int calc1(List<Integer> allNumbers) throws Exception {
-        allNumbers.add(0);
-        Collections.sort(allNumbers);
         int diffOne = 0;
-        int diffThree = 1; // start with one for last diff
+        int diffThree = 0;
         for (int i = 0; i < allNumbers.size() - 1; i++) {
             int diff = allNumbers.get(i + 1) - allNumbers.get(i);
             if (diff == 1) {
@@ -56,10 +51,6 @@ public class Day10 {
     }
 
     public long calc2(List<Integer> allNumbers) {
-        allNumbers.add(0);
-        Collections.sort(allNumbers);
-        allNumbers.add(allNumbers.get(allNumbers.size() - 1) + 3);
-
         Map<Integer, Long> paths = new HashMap<>();
         paths.put(0, 1L);
         for (int i = 1; i < allNumbers.size() - 1; i++) {
