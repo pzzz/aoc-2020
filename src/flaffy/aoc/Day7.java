213e6e4423b4ad9244a3db1a9446c55430e0677e
ulrike_rheinheimer <ulrike.rheinheimer@cas.de>
2020-12-10 16:29:12 +0100

    day 7 task 2

--- src/flaffy/aoc/Day7.java
+++ src/flaffy/aoc/Day7.java
@@ -16,10 +16,10 @@ public class Day7 {
         List<String> allLines = Files.readAllLines(Paths.get("data/ulrike/day7input"));        
         //allLines = Files.readAllLines(Paths.get("data/ulrike/day7testinput"));
         //System.out.println("Ulrike: " + calc1(allLines));
-        // System.out.println("Ulrike: " + calc2(allLines));
+        System.out.println("Ulrike: " + calc2(allLines));
         allLines = Files.readAllLines(Paths.get("data/peter/day7input"));
-        System.out.println("Peter: " + calc1(allLines));
-        // System.out.println("Peter: " + calc2(allLines));
+        //System.out.println("Peter: " + calc1(allLines));
+        System.out.println("Peter: " + calc2(allLines));
         analyse("bright indigo bags contain 4 shiny turquoise bags, 3 wavy yellow bags.","shiny gold");
         
     }
@@ -32,10 +32,7 @@ public class Day7 {
     } 
     
 
-    public int calc2(List<String> allLines) {
-        int count = 0;
-        return count;
-    }
+    
 
     List<String> possibleWrappings = new LinkedList<String>();
 
@@ -69,5 +66,82 @@ public class Day7 {
             }
         }
         return " ";
+    }
+
+    public int calc2(List<String> allLines) {
+        Line [] allRegulations = new Line[allLines.size()];
+        for (int i = 0; i<allLines.size(); i++) {
+            allRegulations[i] = new Line(allLines.get(i));            
+        }
+        return contentNumber ("shiny gold", allRegulations);       
+        
+        
+    }
+
+    //rekursive Funktion zur Ermittlung der enthaltenen Gepäckstücke
+    public int contentNumber (String color, Line[]allRegulations){
+
+        Line line = getRightLine(allRegulations, color); //Zeile die beschreibt, was Farbe enthält
+        int length;
+        try{
+            length = line.contents.length; //Anzahl unterschieliche Inhalte
+        }catch (Exception e){
+            System.out.println("für die Farbe "+color+" scheint es keine Definition zu geben");
+            return 0;
+        }
+
+        int [] countBags = new int[length]; //Zähler pro Zweig
+
+        for(int i = 0; i < length; i++){ 
+            String [] content = line.contents[i]; //eine Farbe + Anzahl
+            try{     
+            countBags[i]=Integer.parseInt(content[0]);
+            int innerResult = contentNumber (content[1], allRegulations);
+            if(innerResult!=1){
+                countBags[i] = countBags[i] + countBags[i]*innerResult;
+            }else{ //keine weitere Verzweigung
+                countBags[i]*=innerResult;
+            }
+            }catch (Exception e){
+                //enthält keine Taschen
+                countBags[i]=1;
+            }            
+        }
+
+        int result=0;
+        for(int i = 0; i<length; i++){
+            result+=countBags[i];
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    
+    public Line getRightLine(Line[]allRegulations, String color){
+        //System.out.println("Farbe "+color);
+        for(Line l: allRegulations){
+            //System.out.println(l.wrapping);            
+            if(l.wrapping.equals(color)){
+                return l;
+            }
+        }
+        System.out.println("das sollte höchstens am ende der rekursion ausgegeben werden");
+        return null;
+    }
+
+    private class Line {
+        private String wrapping;
+        String [][] contents; 
+
+    public Line(String line) {
+        wrapping = line.split(" bags contain ")[0];
+        String [] mixedContent = line.split(" bags contain ")[1].split(" bag[s]?(,[\s]|[.])"); //Inhalte voneinander teilen (jeweils Zahl und Farbe in einer Zelle)
+        contents = new String [mixedContent.length][2];
+        for(int i = 0; i<mixedContent.length;i++){
+            contents [i][0] =mixedContent[i].substring(0,1);
+            contents [i][1] = mixedContent[i].substring(2);            
+        }
+        //System.out.println(contents.length);
+    }
     }
 }
